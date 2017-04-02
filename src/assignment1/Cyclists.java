8a5a53b3a8f9a4b73034f13a67f8322ec5febc72
rmit-s3619792-GUOHUAN-LI <s3619792@student.rmit.edu.au>
2017-04-02 15:18:52 +1000

    Update Cyclists.java

--- src/assignment1/Cyclists.java
+++ src/assignment1/Cyclists.java
@@ -1,5 +1,18 @@
 package assignment1;
 
-public class Cyclists {
+public class Cyclists extends Athlete{
+  private int id;
+  public Cyclists(String_name, String_state, int_age){
+    score=0;
+    name=_name;
+    state=_state;
+    age=_age;
+    ++number;
+    type="cyclists"
+      id=num;
+  }
+  public void compete(){
+    time=(int)(Math.radom()*300)+500;
+  }
 
 }
