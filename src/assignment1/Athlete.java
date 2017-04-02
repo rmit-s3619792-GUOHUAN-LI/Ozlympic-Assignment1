d69751b0ab3412245f5632d9b6e602eb97c6d190
rmit-s3619792-GUOHUAN-LI <s3619792@student.rmit.edu.au>
2017-04-02 15:46:56 +1000

    Add files via upload

---
+++ src/assignment1/Athlete.java
@@ -0,0 +1,10 @@
+public abstract class Athlete{
+    static int num=0;
+    public int time;
+    public int score;
+    public String type;
+    public String name;
+    public int age;
+    public String state;
+    abstract void compete();
+}
---
+++ src/assignment1/Official.java
@@ -0,0 +1,7 @@
+//the Official class
+public class Official{
+    public String name;
+    public Official(String _name){
+        name=_name;
+    }
+}
